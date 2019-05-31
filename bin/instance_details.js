32752d1cb9af6b893b54f830e419986b58636d54
Robert Hafner <tedivm@tedivm.com>
2019-05-31 00:18:34 -0700

    Change region order to match docs, add comments

--- bin/instance_details.js
+++ bin/instance_details.js
@@ -12,22 +12,24 @@ const regionMap = {
   'US East (N. Virginia)': 'us-east-1',
   'US West (N. California)': 'us-west-1',
   'US West (Oregon)': 'us-west-2',
+  'Asia Pacific (Hong Kong)': 'ap-east-1',
   'Asia Pacific (Tokyo)': 'ap-northeast-1',
   'Asia Pacific (Seoul)': 'ap-northeast-2',
   'Asia Pacific (Osaka-Local)': 'ap-northeast-3',
   'Asia Pacific (Mumbai)': 'ap-south-1',
   'Asia Pacific (Singapore)': 'ap-southeast-1',
   'Asia Pacific (Sydney)': 'ap-southeast-2',
-  'Asia Pacific (Hong Kong)': 'ap-east-1',
   'Canada (Central)': 'ca-central-1',
-  'China (Beijing)': 'cn-north-1',
-  'China (Ningxia)': 'cn-northwest-1',
   'EU (Frankfurt)': 'eu-central-1',
   'EU (Ireland)': 'eu-west-1',
   'EU (London)': 'eu-west-2',
   'EU (Paris)': 'eu-west-3',
   'EU (Stockholm)': 'eu-north-1',
   'South America (Sao Paulo)': 'sa-east-1',
+
+  // No pricing currently available.
+  'China (Beijing)': 'cn-north-1',
+  'China (Ningxia)': 'cn-northwest-1',
   'AWS GovCloud (US)': 'us-gov-west-1'
 }
 
