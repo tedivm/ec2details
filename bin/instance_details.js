ed5dd9ab53db7a2e59b9c2cc333fad684742c49e
Robert Hafner <tedivm@tedivm.com>
2019-12-03 10:59:38 -0800

    Add new LA region

--- bin/instance_details.js
+++ bin/instance_details.js
@@ -17,6 +17,7 @@ const regionMap = {
   'US East (N. Virginia)': 'us-east-1',
   'US West (N. California)': 'us-west-1',
   'US West (Oregon)': 'us-west-2',
+  'US West (Los Angeles)': 'us-west-3',
   'Asia Pacific (Hong Kong)': 'ap-east-1',
   'Asia Pacific (Tokyo)': 'ap-northeast-1',
   'Asia Pacific (Seoul)': 'ap-northeast-2',
